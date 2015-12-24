c85372c9b0ae927c856f5d310cabc24346a08b66
luna <stuff@youregoingtolove.me>
2015-12-23 19:44:46 -0600

    add db table creation script

---
+++ db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE members (
+  id VARCHAR(30),
+  username VARCHAR(50)  COLLATE utf8_unicode_ci,
+  lastseen INT(11)
+);
+
+CREATE TABLE data_store (
+  keyword VARCHAR(50) COLLATE utf8_unicode_ci NOT NULL PRIMARY KEY,
+  value VARCHAR(255) COLLATE utf8_unicode_ci,
+  owner VARCHAR(25),
+  approved TINYINT(1) DEFAULT '0'
+);
+
+CREATE TABLE channel_stats (
+  channel VARCHAR(25) PRIMARY KEY,
+  total_messages INT(11),
+  name VARCHAR(25)
+);
