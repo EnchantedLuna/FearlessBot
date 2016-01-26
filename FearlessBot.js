1005d3b19a7c022e82696788a033bbc7074bb465
luna <stuff@youregoingtolove.me>
2016-01-25 19:34:10 -0600

    add more 8ball answers

--- FearlessBot.js
+++ FearlessBot.js
@@ -12,10 +12,11 @@ var db = mysql.createConnection({
 var mybot = new Discord.Client();
 var search;
 
-var eightBallAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it",
-    "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
-    "Better not tell you now", "Cannot predict now", "Concentrate and try again", "Don't count on it", "My reply is no", "My sources say no",
-    "Very doubtful"];
+var eightBallAnswers = ["it is certain", "it is decidedly so", "without a doubt", "yes, definitely", "you may rely on it",
+    "as I see it, yes", "most likely", "outlook good", "yes", "signs point to yes", "reply hazy try again", "ask again later",
+    "better not tell you now", "cannot predict now", "concentrate and try again", "don't count on it", "my reply is no", "my sources say no",
+    "very doubtful", "lol no", "no way", "you have a better chance of getting a Blade selfie", "the probability is the same as Taylor ever ever getting back together (i.e. never)",
+    "http://i.imgur.com/faYfXxE.gif"];
 
 var taylorSwiftSongs = ["Tim McGraw", "Picture to Burn", "Teardrops on My Guitar", "A Place in This World", "Cold As You", "The Outside",
     "Tied Together With a Smile", "Stay Beautiful", "Should've Said No", "Mary's Song", "Our Song", "I'm Only Me When I'm With You",
