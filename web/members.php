96cae1ccbf400335470312f6b8e4741cce33d910
EnchantedLuna <fluttershy@youregoingtolove.me>
2017-01-29 19:38:09 -0600

    DataTables on members page

--- web/members.php
+++ web/members.php
@@ -9,12 +9,36 @@ $db->set_charset("utf8");
 <head>
     <title>/r/TaylorSwift Discord Member Statistics</title>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" href="sortable-theme-bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jq-2.2.4/dt-1.10.13/datatables.min.css"/>
 </head>
 <body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">FearlessBot</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="fearlessdata.php">Saved Items</a></li>
+                    <li><a href="members.php">Members</a></li>
+                    <li><a href="daily.php">Daily Stats</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 <div id="dataStore">
     <h1>Member Statistics</h1>
-    <table class="sortable-theme-bootstrap" data-sortable>
+    <table class="table table-striped" id="memberTable">
         <thead>
         <tr>
             <th>Username</th>
@@ -24,6 +48,15 @@ $db->set_charset("utf8");
             <th>Last Seen</th>
         </tr>
         </thead>
+        <tfoot>
+        <tr>
+            <th>Username</th>
+            <th>Words</th>
+            <th>Messages*</th>
+            <th>Words/Msg</th>
+            <th>Last Seen</th>
+        </tr>
+        </tfoot>
         <tbody>
         <?php
         if (isset($_GET['showinactive']))
@@ -44,7 +77,15 @@ $db->set_charset("utf8");
         </tbody>
     </table>
 </div>
-*messages based on FearlessBot total. This will be less than TaylorBot's count if you have joined before words started being counted.
-<script src="sortable.min.js"></script>
+</div>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jq-2.2.4/dt-1.10.13/datatables.min.js"></script>
+<script>
+    $(document).ready(function() {
+       $('#memberTable').DataTable({
+           "iDisplayLength": 25,
+           "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
+       });
+    });
+</script>
 </body>
 </html>
