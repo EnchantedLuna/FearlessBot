80dc24114218ac0b4d4e3e3ebf0b9e78d6d05aca
EnchantedLuna <stuff@youregoingtolove.me>
2017-08-12 20:18:27 -0500

    fbot2

---
+++ FearlessBot2.js
@@ -0,0 +1,18 @@
+var config = require("./auth.json");
+var Discord = require("discord.js");
+var mysql = require("mysql");
+var bot = new Discord.Client();
+
+var db = mysql.createConnection({
+    host: config.mysqlHost,
+    user: config.mysqlUser,
+    password: config.mysqlPass,
+    database: config.mysqlDB,
+    charset: "utf8mb4"
+});
+
+client.on('ready', () => {
+  console.log('FearlessBot2 is ready.');
+});
+
+bot.loginWithToken(config.token);
