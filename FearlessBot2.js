5554de7538f09545b61c51296b0524333b7b9d97
EnchantedLuna <stuff@youregoingtolove.me>
2017-09-28 19:05:09 -0500

    update config

--- FearlessBot2.js
+++ FearlessBot2.js
@@ -1,4 +1,4 @@
-const config = require("./auth.json");
+const config = require("./config.json");
 const Discord = require("discord.js");
 const mysql = require("mysql");
 const staticData = require("./staticData.json");
