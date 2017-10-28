26a16ce66a1c214ace8ac56c11b403723524c9fd
EnchantedLuna <stuff@youregoingtolove.me>
2017-10-27 22:50:52 -0500

    Shitpost list upgrade

--- web/shitposts.php
+++ web/shitposts.php
@@ -7,28 +7,51 @@ $db->set_charset("utf8mb4");
 <!DOCTYPE html>
 <html>
 <head>
-    <title>FearlessBot Data Storage</title>
+    <title>Shitposts</title>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" href="sortable-theme-dark.css" />
-    <link rel="stylesheet" href="dark.css" />
-    <style type="text/css">
-        #dataStore {
-            margin-bottom: 10px;
-            float:left;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jq-2.2.4/dt-1.10.13/datatables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="darkly.css"/>
 </head>
 <body>
-<div id="dataStore">
-    <h1>FearlessBot Shitposts</h1>
-    <table class="sortable-theme-dark" data-sortable>
-        <thead>
-        <tr>
-            <th>Number</th>
-            <th>Shitpost</th>
-        </tr>
-        </thead>
-        <tbody>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">FearlessBot</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="fearlessdata.php">Saved Items</a></li>
+                    <li><a href="members.php">Members</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+    <div id="shitposts">
+        <h1>Shitposts</h1>
+        <table class="table table-striped" id="memberTable">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Shitpost</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <th>#</th>
+                <th>Shitpost</th>
+            </tr>
+            </tfoot>
+            <tbody>
         <?php
         $query = $db->query("SELECT * FROM shitposts ORDER BY id");
         while ($row = $query->fetch_array())
@@ -37,9 +60,18 @@ $db->set_charset("utf8mb4");
         }
         ?>
         </tbody>
-    </table>
+        </table>
+    </div>
 </div>
-<script src="sortable.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jq-2.2.4/dt-1.10.13/datatables.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#memberTable').DataTable({
+            "iDisplayLength": 25,
+            "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
+        });
+    });
+</script>
 </body>
 </html>
 
