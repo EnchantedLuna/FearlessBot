c0812dca463919e9a84feb7dd864b0dbf438fd54
EnchantedLuna <stuff@youregoingtolove.me>
2017-11-22 22:19:26 -0600

    Awards

--- FearlessBot2.js
+++ FearlessBot2.js
@@ -214,6 +214,9 @@ bot.on('message', message => {
         case "!rankpoop":
             rankThingCommand(message, "poops", parseInt(command[1]));
             break;
+        case "!awards":
+            awardsCommand(message);
+            break;
 
         // Mod commands
         case "!approve":
@@ -785,6 +788,34 @@ function lorpointsCommand(message, params)
             });
         }
     });
+}
+
+function awardsCommand(message)
+{
+    let member;
+    if (message.mentions.members.size > 0) {
+        member = message.mentions.members.first().user;
+    } else {
+        member = message.author;
+    }
+
+    db.query("SELECT * FROM awards WHERE server = ? AND member = ? ORDER BY date, id",
+        [message.channel.guild.id, member.id], function (err, rows) {
+            if (err != null) {
+                console.log(err);
+                return;
+            }
+            if (rows[0] != null) {
+                let awardsText = '\nAwards for ' + member.username + ':\n';
+                for (let i = 0; i < rows.length; i++) {
+                    let date = rows[i].date.toDateString();
+                    awardsText += (i+1) + ". " + rows[i].award + " [" + date + "]\n";
+                }
+                message.reply(awardsText);
+            } else {
+                message.reply("no awards :(");
+            }
+        });
 }
 
 
