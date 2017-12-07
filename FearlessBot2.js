3b81ce7c4d5aebe323a22e9699c8ddcd54b38577
EnchantedLuna <stuff@youregoingtolove.me>
2017-12-06 22:01:43 -0600

    mods command

--- FearlessBot2.js
+++ FearlessBot2.js
@@ -208,6 +208,9 @@ bot.on('message', message => {
         case "!awards":
             awardsCommand(message);
             break;
+        case "!mods":
+            modsCommand(message);
+            break;
 
         // Mod commands
         case "!approve":
@@ -997,6 +1000,30 @@ function regionCommand(message, region)
             message.reply("region not recognized. Acceptable values: northamerica, southamerica, europe, asia, africa, oceania, clear.");
         break;
     }
+}
+
+function modsCommand(message)
+{
+    let mods = message.channel.guild.roles.find('name','mods');
+    if (mods === 'undefined') {
+        return;
+    }
+    let joinDate = message.member.joinedAt;
+    let now = new Date();
+    let joinTime = (now.getTime() - joinDate.getTime()) / 1000;
+    if (joinTime < 60*60*24*14) {
+        message.reply('this command can only be used by members who have joined more than 2 weeks ago.');
+        return;
+    }
+
+    mods.setMentionable(true, 'activated by ' + message.author.username);
+    log(message.channel.guild, 'Mods tag activated by '+ message.author.username);
+    message.reply("the mods tag has activated. Do not continue if this is not a serious issue that needs attention and no mods are currently active. " +
+        "Otherwise, use the tag quickly, as it will be disabled in 2 minutes.");
+    setTimeout(function() {
+        mods.setMentionable(false);
+        log(message.channel.guild, 'Mods tag deactivated.');
+    }, 120000);
 }
 
 
