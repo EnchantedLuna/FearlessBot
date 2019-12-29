a5a7d0e0eb1e03b37a9a80db927fa3968c2b873d
EnchantedLuna <stuff@youregoingtolove.me>
2019-12-28 23:17:58 -0600

    width tweaks

--- web/activityreport.php
+++ web/activityreport.php
@@ -61,6 +61,6 @@ while ($query->fetch()) {
 
 <body>
 <!--Div that will hold the pie chart-->
-<div id="chart_div" style="width: 100%;height:500px"></div>
+<div id="chart_div" style="width: 1000px;height:500px"></div>
 </body>
 </html>
