fa5771c343bcce8cede266dba8ce393e0fe03083
EnchantedLuna <stuff@youregoingtolove.me>
2020-07-14 22:44:41 -0500

    remove messages from db template

--- db.sql
+++ db.sql
@@ -43,18 +43,7 @@ CREATE TABLE names
   gender CHAR(1),
   year INT(4)
 );
-CREATE TABLE messages
-(
-  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  discord_id BIGINT(20),
-  date DATETIME,
-  server BIGINT(20),
-  channel BIGINT(20),
-  edited DATETIME,
-  author BIGINT(20)
-);
-CREATE INDEX idx_author ON messages (server, author);
-CREATE INDEX idx_messages ON messages (server);
+
 CREATE TABLE shitposts
 (
   id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -72,8 +61,6 @@ CREATE TABLE scheduled_actions
     effectivetime DATETIME,
     completed TINYINT(1) NOT NULL DEFAULT 0
 );
-
-CREATE VIEW `message_view` AS select `msg`.`id` AS `id`,`mem`.`username` AS `username`,`msg`.`date` AS `date`,`c`.`name` AS `channelname`,`msg`.`message` AS `message` from ((`messages` `msg` join `members` `mem` on(((`msg`.`server` = `mem`.`server`) and (`msg`.`author` = `mem`.`id`)))) join `channel_stats` `c` on(((`msg`.`server` = `c`.`server`) and (`msg`.`channel` = `c`.`channel`))));
 
 CREATE TABLE mod_stats
 (
