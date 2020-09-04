8ea19965b64185dd9ca8cbbe31e9d6bc74ed38cc
EnchantedLuna <stuff@youregoingtolove.me>
2020-09-03 22:21:50 -0500

    channel activity chart

--- web/activityreport.php
+++ web/activityreport.php
@@ -18,6 +18,21 @@ while ($query->fetch()) {
     $counts[$month."/".$year] += $count;
 }
 
+$query = $db->prepare("SELECT name, SUM(message_count) 
+FROM user_message_stats ums
+JOIN channel_stats cs ON ums.guild=cs.server AND ums.channel=cs.channel
+WHERE ums.guild = ? AND user = ? AND web=1
+GROUP BY name
+");
+$query->bind_param('ss', $_GET['server'], $_GET['user']);
+$query->execute();
+$query->bind_result($channel, $messageCount);
+
+$channelCounts = array();
+while ($query->fetch()) {
+    $channelCounts[$channel] = $messageCount;
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,6 +40,7 @@ while ($query->fetch()) {
     <title>Activity Report</title>
     <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
     <script type="text/javascript">
 
         // Load the Visualization API and the piechart package.
@@ -63,5 +79,21 @@ while ($query->fetch()) {
 <body>
 <!--Div that will hold the pie chart-->
 <div id="chart_div" style="width: 1000px;height:500px"></div>
+<canvas id="myChart" width="700" height="500"></canvas>
+<script>
+    var data = {
+        datasets: [{
+            data: <?php echo json_encode(array_values($channelCounts)); ?>,
+            backgroundColor: ['#b2cefe', '#a1836b', '#fea3aa', '#a55168', '#f8b88b', '#b19cd9', '#f47fff', '#F1C40F', '#7BE3BB', '#eacdd0']
+        }],
+
+        labels: <?php echo json_encode(array_keys($channelCounts));?>
+    };
+    var myDoughnutChart = new Chart('myChart', {
+    type: 'doughnut',
+    data: data,
+    options: {responsive: false}
+});
+</script>
 </body>
 </html>
