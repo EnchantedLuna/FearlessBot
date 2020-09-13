e3b3d3bcef2e08cd005d483ddf481e5ead87c6d6
EnchantedLuna <stuff@youregoingtolove.me>
2020-09-13 16:53:55 -0500

    update lorpoint award message

--- commands/award.js
+++ commands/award.js
@@ -4,6 +4,28 @@ exports.run = function (message, args, bot, db) {
   let pieces = args.split(" ");
   let number = parseInt(pieces[0]);
   let list = [];
+
+  if (isNaN(number)) {
+    message.channel.send("", {
+      embed: {
+        description:
+          ":x: You must enter a valid number of lorpoints to award.\nUsage: ``!award [number] @user1 @user2``",
+        color: 0xff0000,
+      },
+    });
+    return;
+  }
+
+  if (message.mentions.members.size === 0) {
+    message.channel.send("", {
+      embed: {
+        description: ":x: You must mention a member to award lorpoints to.",
+        color: 0xff0000,
+      },
+    });
+    return;
+  }
+
   message.mentions.members.forEach(function (member, key, map) {
     db.query(
       "UPDATE members SET lorpoints=lorpoints+? WHERE server = ? AND id = ?",
@@ -13,9 +35,14 @@ exports.run = function (message, args, bot, db) {
   });
   let finalList = list.join(", ");
   let lorpointWord = number !== 1 ? "lorpoints" : "lorpoint";
-  message.channel.send(
-    number + " " + lorpointWord + " have been awarded to: " + finalList
-  );
+  message.channel.send("", {
+    embed: {
+      title: ":star: Awarding Lorpoints",
+      description:
+        number + " " + lorpointWord + " have been awarded to:\n" + finalList,
+      color: 0xdbe07e,
+    },
+  });
   log(
     message.channel.guild,
     number +
