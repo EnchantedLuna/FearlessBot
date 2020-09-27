5e5a7dc44e8c736dda097e0a79c03dded716f8fd
EnchantedLuna <stuff@youregoingtolove.me>
2020-09-26 21:00:47 -0500

    supermute role validation

--- commands/supermute.js
+++ commands/supermute.js
@@ -3,18 +3,29 @@ const { isMod } = require("../util");
 exports.run = function (message, args, bot, db) {
   const argSet = args.split(" ");
   const hours = parseInt(argSet[0]);
-  let supermute = message.channel.guild.roles.cache.find(
+  const supermute = message.channel.guild.roles.cache.find(
     (role) => role.name === "supermute"
   );
-  let active = message.channel.guild.roles.cache.find(
+  const active = message.channel.guild.roles.cache.find(
     (role) => role.name === "active"
   );
+  if (!supermute) {
+    message.channel.send("", {
+      embed: {
+        description:
+          ":warning: Could not find ``supermute`` role. A role named ``supermute`` is required to use this command.",
+      },
+    });
+    return;
+  }
   message.mentions.members.forEach(function (member, key, map) {
     if (isMod(member, message.channel.guild)) {
       message.channel.send(":smirk:");
     } else {
       member.roles.add(supermute);
-      member.roles.remove(active);
+      if (active) {
+        member.roles.remove(active);
+      }
       var timeMessage = "";
       if (hours > 0) {
         db.query(
