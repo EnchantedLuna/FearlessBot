d8f81b9f7f1f06dcc262fa9e3b79414eecb947d9
EnchantedLuna <stuff@youregoingtolove.me>
2020-09-27 21:08:41 -0500

    clear unmute action if manually unmuting

--- commands/unsupermute.js
+++ commands/unsupermute.js
@@ -1,4 +1,4 @@
-exports.run = function (message) {
+exports.run = function (message, args, bot, db) {
   let supermute = message.channel.guild.roles.cache.find(
     (role) => role.name === "supermute"
   );
@@ -7,6 +7,10 @@ exports.run = function (message) {
   }
   message.mentions.members.forEach(function (member, key, map) {
     member.roles.remove(supermute);
+    db.query(
+      "UPDATE scheduled_actions SET completed=1 WHERE guild = ? AND user = ? AND action='unmute'",
+      [message.channel.guild.id, member.id]
+    );
     message.channel.send(member.user.tag + " has been un-supermuted.");
   });
 };
