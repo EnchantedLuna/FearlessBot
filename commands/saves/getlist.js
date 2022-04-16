669e71a3a4adc45bb50f6165c56d7c44490f1fbb
Rachel <web@tay.rocks>
2022-04-16 09:59:19 -0700

    getlist as slash command

--- commands/saves/getlist.js
+++ commands/saves/getlist.js
@@ -5,3 +5,10 @@ exports.run = function (message) {
     config.baseUrl + "fearlessdata.php?server=" + message.channel.guild.id
   );
 };
+
+exports.interaction = function (interaction, bot, db) {
+  interaction.reply({
+    content: config.baseUrl + "fearlessdata.php?server=" + interaction.guild.id,
+    ephemeral: true
+  });
+};
