9a2a55436c9399ce3bd7db3dbf65c3fb8c034ea6
Rachel <web@tay.rocks>
2022-04-16 09:41:50 -0700

    choose as slash command

--- commands/fun/choose.js
+++ commands/fun/choose.js
@@ -1,22 +1,33 @@
 const { MessageEmbed } = require("discord.js");
-exports.run = function (message, args, bot, db) {
-  let choices = args.split(",");
+
+function choose(choices, user)
+{
   if (choices.length > 1) {
     let selectedChoice = choices[Math.floor(Math.random() * choices.length)];
-    message.channel.send({
+    return {
       embeds: [
           new MessageEmbed()
-              .setTitle("Choosing for " + message.author.tag)
+              .setTitle("Choosing for " + user)
               .setDescription("I pick: " + selectedChoice.trim())
       ]
-    });
+    };
   } else {
-    message.channel.send({
+    return {
       embeds: [
           new MessageEmbed()
-              .setTitle("Choosing for " + message.author.tag)
+              .setTitle("Choosing for " + user)
               .setDescription(":warning: Usage: ``!choose option1, option2, option3``")
       ]
-    });
+    };
   }
+}
+
+exports.run = function (message, args, bot, db) {
+  const choices = args.split(",");
+  message.channel.send(choose(choices, message.author.tag));
 };
+
+exports.interaction = function(interaction, bot, db) {
+  const choices = interaction.options.getString("choices").split(",");
+  interaction.reply(choose(choices, interaction.user.tag));
+}
