daae8c66923061d295df926731ed6ad2adcb4317
Rachel <web@tay.rocks>
2022-04-29 22:50:36 -0700

    namemix slash command

--- commands/bot/screate.js
+++ commands/bot/screate.js
@@ -5,6 +5,7 @@ exports.run = function (message, args, bot, db, globalScope) {
         const scope = (globalScope) ? bot.application.commands : bot.guilds.cache.get(message.channel.guild.id).commands;
         if (commands[args].interactionCreate) {
             void scope.create(commands[args].interactionCreate);
+            message.channel.send(":star:");
         }
     }
 }
--- commands/fun/namemix.js
+++ commands/fun/namemix.js
@@ -10,3 +10,16 @@ exports.run = function (message, args, bot, db) {
     }
   );
 };
+
+exports.interaction = function(interaction, bot, db) {
+  db.query(
+    "SELECT CONCAT((SELECT name_piece FROM namemix WHERE part=1 ORDER BY RAND() LIMIT 1), " +
+      "(SELECT name_piece FROM namemix WHERE part=2 ORDER BY RAND() LIMIT 1)) AS name",
+    [],
+    function (err, rows) {
+      interaction.reply({
+        embeds: [{ title: "Name Mix", description: rows[0].name }],
+      });
+    }
+  );
+}
