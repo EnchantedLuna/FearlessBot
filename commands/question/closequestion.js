0c36bcc338125f92b6e5ce81cacb84d1d77a7615
Rachel <web@tay.rocks>
2022-09-30 21:58:18 -0700

    open/close questions slash command

--- commands/question/closequestion.js
+++ commands/question/closequestion.js
@@ -1,21 +1,29 @@
 const config = require("../../config.json");
-exports.run = function (message, params, bot, db) {
-  db.query("SELECT * FROM trivia_questions WHERE id = ?", [params], function (
-    err,
-    rows
-  ) {
-    if (rows[0] == null) {
-      message.reply("Invalid question id");
-      return;
-    }
-    if (
-      rows[0].user !== message.author.id &&
-      message.author.id !== config.botAdminUserId
-    ) {
-      message.reply("You do not have permission to close this question.");
-      return;
-    }
-    db.query("UPDATE trivia_questions SET isopen=0 WHERE id = ?", [params]);
-    message.reply("Question #" + params + " has been closed.");
-  });
+async function closeQuestion(db, userId, questionNumber) {
+  const [rows] = await db
+    .promise()
+    .query("SELECT * FROM trivia_questions WHERE id = ?", [questionNumber]);
+  if (!rows[0]) {
+    return "Invalid question number.";
+  }
+  if (rows[0].user !== userId && userId !== config.botAdminUserId) {
+    return "You do not have permission to close this question.";
+  }
+  await db
+    .promise()
+    .query("UPDATE trivia_questions SET isopen=0 WHERE id = ?", [
+      questionNumber,
+    ]);
+  return "Question #" + questionNumber + " has been closed.";
+}
+
+exports.run = async function (message, params, bot, db) {
+  const result = await closeQuestion(db, message.author.id, params);
+  message.reply(result);
+};
+
+exports.interaction = async function (interaction, bot, db) {
+  const id = interaction.options.getInteger("id");
+  const result = await closeQuestion(db, interaction.user.id, id);
+  interaction.reply(result);
 };
