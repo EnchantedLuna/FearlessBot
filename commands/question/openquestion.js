d7fbb90db5830d01d8f79e8fb6b166071908a41f
Rachel <web@tay.rocks>
2022-09-30 22:42:09 -0700

    add openquestion slash

--- commands/question/openquestion.js
+++ commands/question/openquestion.js
@@ -1,21 +1,29 @@
 const config = require("../../config.json");
-exports.run = function (message, params, bot, db) {
-  db.query("SELECT * FROM trivia_questions WHERE id = ?", [params], function (
-    err,
-    rows
-  ) {
-    if (rows[0] == null) {
-      message.reply("Invalid question id");
-      return;
-    }
-    if (
-      rows[0].user !== message.author.id &&
-      message.author.id !== config.botAdminUserId
-    ) {
-      message.reply("You do not have permission to open this question.");
-      return;
-    }
-    db.query("UPDATE trivia_questions SET isopen=1 WHERE id = ?", [params]);
-    message.reply("Question #" + params + " has been opened.");
-  });
+async function openQuestion(db, userId, questionNumber) {
+  const [rows] = await db
+    .promise()
+    .query("SELECT * FROM trivia_questions WHERE id = ?", [questionNumber]);
+  if (!rows[0]) {
+    return "Invalid question number.";
+  }
+  if (rows[0].user !== userId && userId !== config.botAdminUserId) {
+    return "You do not have permission to open this question.";
+  }
+  await db
+    .promise()
+    .query("UPDATE trivia_questions SET isopen=1 WHERE id = ?", [
+      questionNumber,
+    ]);
+  return "Question #" + questionNumber + " has been opened.";
+}
+
+exports.run = async function (message, params, bot, db) {
+  const result = await openQuestion(db, message.author.id, params);
+  message.reply(result);
+};
+
+exports.interaction = async function (interaction, bot, db) {
+  const id = interaction.options.getInteger("id");
+  const result = await openQuestion(db, interaction.user.id, id);
+  interaction.reply(result);
 };
