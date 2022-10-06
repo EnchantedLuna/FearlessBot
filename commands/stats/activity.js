a6818a1d77d21eaf642924dfda0eafd5e29f09db
Rachel <web@tay.rocks>
2022-10-05 22:35:55 -0700

    channel stats in /activity

--- commands/stats/activity.js
+++ commands/stats/activity.js
@@ -1,37 +1,60 @@
 const config = require("../../config.json");
 const { findMember } = require("../../util");
 
+async function getStats(db, guild, member) {
+  const [rows] = await db
+    .promise()
+    .query(
+      "SELECT name, SUM(message_count) AS count FROM user_message_stats ums \
+  JOIN channel_stats cs ON ums.guild=cs.server AND ums.channel=cs.channel \
+  WHERE ums.guild = ? AND user = ? AND web=1 GROUP BY name ORDER BY count DESC",
+      [guild, member]
+    );
+  let stats = "Messages by channel:\n";
+  if (rows.length === 0) {
+    stats += "(none)\n";
+  }
+  for (let row of rows) {
+    const line = row.name + " - " + row.count + "\n";
+    stats += line;
+  }
+  stats += "Chart and monthly stats: " + getStatsLink(guild, member);
+
+  return stats;
+}
+
+function getStatsLink(guild, member) {
+  return (
+    config.baseUrl + "activityreport.php?server=" + guild + "&user=" + member
+  );
+}
+
 exports.run = async function (message, args, bot, db) {
   const member = await findMember(message, args, bot);
-  const botsString = message.content.includes("bots")
-    ? "&includebots=true"
-    : "";
-  const url =
-    config.baseUrl +
-    "activityreport.php?server=" +
-    message.channel.guild.id +
-    "&user=" +
-    member.id +
-    botsString;
+  const description = await getStats(db, message.channel.guild.id, member.id);
   message.channel.send({
-    embeds: [{
-      title: "Activity Report for " + member.displayName,
-      description: url,
-    }],
+    embeds: [
+      {
+        title: "Activity Report for " + member.displayName,
+        description: description,
+      },
+    ],
   });
 };
 
-exports.interaction = function(interaction, bot, db) {
+exports.interaction = async function (interaction, bot, db) {
   let member = interaction.options.getMember("member");
   if (!member) {
     member = interaction.member;
   }
 
-  const url = config.baseUrl + "activityreport.php?server=" + interaction.guild.id + "&user=" + member.id;
+  const description = await getStats(db, interaction.guild.id, member.id);
   interaction.reply({
-    embeds: [{
-      title: "Activity Report for " + member.displayName,
-      description: url,
-    }],
+    embeds: [
+      {
+        title: "Activity Report for " + member.displayName,
+        description: description,
+      },
+    ],
   });
-}
+};
