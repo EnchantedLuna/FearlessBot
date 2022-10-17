5155e92d354ae47d9b240e5023421cd330dcc066
Rachel <web@tay.rocks>
2022-10-16 20:25:20 -0700

    upgrade !words to slash command

--- commands/stats/words.js
+++ commands/stats/words.js
@@ -1,36 +1,43 @@
 const { findMemberID } = require("../../util");
 
-exports.run = function (message, params, bot, db) {
-  const member = findMemberID(message, params, bot);
-
-  db.query(
-    "SELECT words, messages, username FROM members WHERE server = ? AND id = ?",
-    [message.channel.guild.id, member],
-    function (err, rows) {
-      if (err != null) {
-        console.log(err);
-        return;
-      }
-      if (rows[0] != null) {
-          const average =
-              rows[0].messages > 0
-                  ? Math.round((rows[0].words / rows[0].messages) * 100) / 100
-                  : 0;
-          message.channel.send(
-          rows[0].username +
-            " has used " +
-            rows[0].words +
-            " words in " +
-            rows[0].messages +
-            " messages, an average of " +
-            average +
-            " words per message."
-        );
-      } else {
-        message.channel.send(
-          ":warning: User not found. Please double check the username."
-        );
-      }
-    }
+async function getWords(db, guildId, memberId) {
+  const [rows] = await db
+    .promise()
+    .query(
+      "SELECT words, messages, username FROM members WHERE server = ? AND id = ?",
+      [guildId, memberId]
+    );
+  if (!rows[0]) {
+    return "Member not found.";
+  }
+  const average =
+    rows[0].messages > 0
+      ? Math.round((rows[0].words / rows[0].messages) * 100) / 100
+      : 0;
+  return (
+    rows[0].username +
+    " has used " +
+    rows[0].words +
+    " words in " +
+    rows[0].messages +
+    " messages, an average of " +
+    average +
+    " words per message."
   );
+}
+
+exports.run = async function (message, params, bot, db) {
+  const member = findMemberID(message, params, bot);
+  message.channel.send(await getWords(db, message.channel.guild, member));
+};
+
+exports.interaction = async function (interaction, bot, db) {
+  let member = interaction.options.getMember("member");
+  if (!member) {
+    member = interaction.member;
+  }
+  const response = await getWords(db, interaction.guild.id, member.id);
+  interaction.reply({
+    embeds: [{ title: "Word Count", description: response }],
+  });
 };
