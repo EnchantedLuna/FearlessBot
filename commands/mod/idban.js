4238222c4b2e684212295bf51f6038b2843294e9
Rachel <web@tay.rocks>
2022-10-23 13:29:49 -0700

    idban slash command

--- commands/mod/idban.js
+++ commands/mod/idban.js
@@ -8,9 +8,49 @@ exports.run = function (message, userId) {
       .ban(userId)
       .then((user) => {
         message.channel.send({
-          embeds: [{
-            description: `:hammer: Banned ${user.username || user.id || user}.`,
-          }],
+          embeds: [
+            {
+              description: `:hammer: Banned ${
+                user.username || user.id || user
+              }.`,
+            },
+          ],
+        });
+      })
+      .catch((error) => {
+        message.channel.send(
+          ":no_entry: There was an error executing this operation."
+        );
+      });
+  }
+};
+
+exports.interaction = function (interaction, bot, db) {
+  const userId = interaction.options.getString("id");
+  const days = interaction.options.getInteger("days");
+  if (isMod(userId, interaction.guild)) {
+    interaction.reply(":smirk:");
+  } else {
+    interaction.guild.members
+      .ban(userId)
+      .then((user) => {
+        let responseText = `:hammer: Banned ${
+          user.username || user.id || user
+        }`;
+        if (days > 0) {
+          db.query(
+            "INSERT INTO scheduled_actions (action, guild, user, effectivetime) \
+                    VALUES ('unban', ?, ?, NOW() + INTERVAL ? DAY)",
+            [interaction.guild.id, userId, days]
+          );
+          responseText += ` for ${days} days`;
+        }
+        interaction.reply({
+          embeds: [
+            {
+              description: responseText,
+            },
+          ],
         });
       })
       .catch((error) => {
