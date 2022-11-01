52f154fdd24282d06686a9f3a30e45333ebe0a1d
Rachel <web@tay.rocks>
2022-11-01 00:26:05 -0700

    Revert "fix length handling"
    This reverts commit 6ccaf451ccd22dfbebfd8904140aa5928e44a38f.

--- commands/saves/get.js
+++ commands/saves/get.js
@@ -45,10 +45,6 @@ async function getItem(
   }
   embed.footer = { text: date };
 
-  if (text.match()) {
-    embed.
-  }
-
   if (text.match("^(http(s?):)([/|.|\\w|\\s|-])*\\.(?:jpg|gif|png)$")) {
     embed.image = { url: text };
     return embed;
