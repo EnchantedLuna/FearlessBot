b77f9f61d58eef702475f6ea184fdd84fb2595d6
Rachel <web@tay.rocks>
2022-11-22 20:26:15 -0800

    ban slash command

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -2,7 +2,7 @@ const { isMod, log } = require("../../util");
 
 exports.run = function (message, args, bot, db) {
   const argSet = args.split(" ");
-  const days = parseInt(argSet[0]);
+  const days = parseInt(argSet[0]) ?? 0;
   message.mentions.members.forEach(function (member, key, map) {
     if (isMod(member, message.channel.guild)) {
       message.channel.send(":smirk:");
@@ -60,4 +60,65 @@ exports.run = function (message, args, bot, db) {
         message.author.username
     );
   });
+};
+
+exports.interaction = function (interaction, bot, db) {
+  const member = interaction.options.getMember("member");
+  const reason = interaction.options.getString("reason");
+  const days = interaction.options.getInteger("days");
+
+  if (isMod(member, interaction.guild)) {
+    interaction.reply(":smirk:");
+    return;
+  }
+
+  if (!member.bannable) {
+    interaction.reply({
+      embeds: [
+        {
+          description: ":warning: I don't have permission to ban that member.",
+        },
+      ],
+    });
+    return;
+  }
+  member.ban({ reason: reason ?? "" });
+  interaction.reply({
+    embeds: [
+      {
+        description:
+          ":hammer: " +
+          member.user.username +
+          "#" +
+          member.user.discriminator +
+          " (" +
+          member.user.id +
+          ")" +
+          " has been banned.",
+      },
+    ],
+  });
+  let timeMessage = "indefinitely";
+  if (days > 0) {
+    db.query(
+      "INSERT INTO scheduled_actions (action, guild, user, effectivetime) \
+                  VALUES ('unban', ?, ?, NOW() + INTERVAL ? DAY)",
+      [interaction.guild.id, member.user.id, days]
+    );
+    timeMessage = "for " + days + " day";
+    timeMessage += days !== 1 ? "s" : "";
+  }
+  log(
+    interaction.guild,
+    member.user.username +
+      "#" +
+      member.user.discriminator +
+      " (" +
+      member.user.id +
+      ")" +
+      " has been banned " +
+      timeMessage +
+      " by " +
+      interaction.user.username
+  );
 };
