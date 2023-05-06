943e867afed3f355fd9a0658adb466f56fd0296a
iAndrewC <andrewc2@users.noreply.github.com>
2023-05-06 12:20:52 -0400

    added a message.inGuild() check too

--- activeRole.js
+++ activeRole.js
@@ -3,6 +3,7 @@ const util = require("./util");
 
 exports.checkActiveRole = async function (message, guild, db) {
   if (
+    message.inGuild() &&
     message.member &&
     message.channel.guild.id === config.mainServer &&
     !hasRole(message.member, message.channel.guild, "active")
