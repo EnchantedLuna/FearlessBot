712bd739ce0264fd17e42b1544b9855c3f0c15ce
Rachel <web@tay.rocks>
2023-07-28 22:52:12 -0700

    improved mods command

--- commands/mod/mods.js
+++ commands/mod/mods.js
@@ -1,4 +1,16 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 const { log } = require("../../util");
+
+const buttons = new MessageActionRow().addComponents(
+  new MessageButton()
+    .setCustomId("allMods")
+    .setLabel("Ping All Mods")
+    .setStyle("PRIMARY"),
+  new MessageButton()
+    .setCustomId("cancel")
+    .setLabel("Cancel")
+    .setStyle("SECONDARY")
+);
 
 exports.run = function (message) {
   let mods = message.channel.guild.roles.cache.find(
@@ -21,7 +33,7 @@ exports.run = function (message) {
     message.reply("Error: I don't have permission to edit the mods role.");
     return;
   }
-  
+
   mods.setMentionable(true, "activated by " + message.author.username);
   log(
     message.channel.guild,
@@ -35,4 +47,67 @@ exports.run = function (message) {
     mods.setMentionable(false);
     log(message.channel.guild, "Mods tag deactivated.");
   }, 120000);
+};
+
+exports.interaction = async function (interaction, bot, db) {
+  let mods = interaction.channel.guild.roles.cache.find(
+    (role) => role.name === "mods"
+  );
+  if (mods === "undefined") {
+    interaction.reply("error: mod role not found");
+    return;
+  }
+
+  let joinDate = interaction.member.joinedAt;
+  let now = new Date();
+  let joinTime = (now.getTime() - joinDate.getTime()) / 1000;
+  if (joinTime < 60 * 60 * 24 * 14) {
+    interaction.reply(
+      "This command can only be used by members who have joined more than 2 weeks ago."
+    );
+    return;
+  }
+
+  const response = await interaction.reply({
+    content:
+      "Are you sure you want to ping mods? Please only do this if there is an issue that needs attention and there are no mods currently active.",
+    components: [buttons],
+    fetchReply: true,
+  });
+
+  const opFilter = (i) => i.user.id === interaction.user.id;
+
+  try {
+    const confirmation = await response.awaitMessageComponent({
+      filter: opFilter,
+      time: 30_000,
+    });
+
+    if (confirmation.customId === "allMods") {
+      await confirmation.update({
+        content: `Okay, pinging the mods.`,
+        components: [],
+      });
+      await interaction.guild.members.fetch();
+      const modList = mods.members.map((m) => "<@" + m.id + ">");
+      interaction.channel.send(
+        "You have been summoned by " +
+          interaction.user.username +
+          ".\n" +
+          modList.join(" ")
+      );
+    } else if (confirmation.customId === "cancel") {
+      await confirmation.update({
+        content: "Okay, I won't ping the mods.",
+        components: [],
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    await interaction.editReply({
+      content:
+        "This mods ping action has been cancelled due to lack of action.",
+      components: [],
+    });
+  }
 };
