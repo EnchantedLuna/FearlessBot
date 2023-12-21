e08394b2c6c24c2a5e39301e58c25a24569ff0b8
Rachel <web@tay.rocks>
2023-12-21 11:06:26 -0800

    remove namemix table

--- db.sql
+++ db.sql
@@ -71,15 +71,6 @@ CREATE TABLE user_message_stats
   PRIMARY KEY (user, guild, channel)
 );
 
-CREATE TABLE namemix
-(
-  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name_piece VARCHAR(20) NOT NULL,
-  part TINYINT(1) NOT NULL,
-  addedon DATETIME,
-  addedby BIGINT(20)
-);
-
 CREATE TABLE trivia_questions
 (
   id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
