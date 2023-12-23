4117c82ffdb57f71df3a341c62a6a631da779b89
Rachel <web@tay.rocks>
2023-12-23 11:01:49 -0800

    make /acronym ephemeral when not found

--- commands/fun/acronym.js
+++ commands/fun/acronym.js
@@ -5,15 +5,26 @@ function getAnswer(searchTerm) {
   if (capitalizedSearchTerm in acronyms) {
     return searchTerm + " refers to " + acronyms[capitalizedSearchTerm];
   }
-  return "I don't have a definition for " + searchTerm + ", sorry!";
+  return "";
 }
 
 exports.run = function (message, args, bot, db) {
+  let answer = getAnswer(args);
+  if (answer == "") {
+    message.reply("I don't have an answer for that, sorry!");
+    return;
+  }
   message.reply(getAnswer(args));
 };
 
 exports.interaction = function (interaction, bot, db) {
   const searchTerm = interaction.options.getString("term");
   const answer = getAnswer(searchTerm);
+  if (answer == "") {
+    interaction.reply({
+      content: "I don't have a definition for " + searchTerm + ", sorry!",
+      ephemeral: true,
+    });
+  }
   interaction.reply({ content: answer });
 };
