df0ff4ca2b631654f2fad8354227295b8d57c9c5
Rachel <web@tay.rocks>
2024-01-14 23:18:37 -0800

    custom timeout

--- commands/mod/supermute.js
+++ commands/mod/supermute.js
@@ -11,10 +11,12 @@ exports.run = function (message, args, bot, db) {
   );
   if (!supermute) {
     message.channel.send({
-      embeds: [{
-        description:
-          ":warning: Could not find ``supermute`` role. A role named ``supermute`` is required to use this command.",
-      }],
+      embeds: [
+        {
+          description:
+            ":warning: Could not find ``supermute`` role. A role named ``supermute`` is required to use this command.",
+        },
+      ],
     });
     return;
   }
@@ -46,5 +48,50 @@ exports.run = function (message, args, bot, db) {
           "."
       );
     }
+  });
+};
+
+exports.interaction = function (interaction, bot, db) {
+  const member = interaction.options.getMember("member");
+  const days = interaction.options.getInteger("days") ?? 0;
+  const hours = interaction.options.getInteger("hours") ?? 0;
+  const reason =
+    interaction.options.getString("reason") ?? "(no reason provided)";
+  if (days == 0 && hours == 0) {
+    interaction.reply("A time interval must be set.");
+    return;
+  }
+  const totalHours = days * 24 + hours;
+  const interval = totalHours * 3600 * 1000;
+  if (!member.moderatable) {
+    interaction.reply({
+      embeds: [
+        {
+          description:
+            ":warning: I don't have permission to timeout that member.",
+        },
+      ],
+    });
+    return;
+  }
+  if (
+    member.roles.highest.position >= interaction.member.roles.highest.position
+  ) {
+    interaction.reply("You do not have permission to timeout that member.");
+    return;
+  }
+  member.timeout(interval, "By " + interaction.user.username + ": " + reason);
+  interaction.reply({
+    embeds: [
+      {
+        description:
+          ":alarm_clock: " +
+          member.user.username +
+          " (" +
+          member.user.id +
+          ")" +
+          " has been timed out.",
+      },
+    ],
   });
 };
