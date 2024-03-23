cedba82299703f1fd5458ce4d85ea571f338b815
Rachel <16313442+EnchantedLuna@users.noreply.github.com>
2024-03-23 11:57:39 -0700

    role ping command

--- commands/mod/events.js
+++ commands/mod/events.js
@@ -1,3 +1,27 @@
+exports.interaction = async function (interaction, bot, db) {
+  const message = interaction.options.getString("message");
+  const role = interaction.options.getRole("role");
+  if (!role) {
+    interaction.reply(":x: I could not find the " + roleName + " role.");
+    return;
+  }
+  if (!role.editable) {
+    interaction.reply(":x: I do not have permission to edit this role.");
+    return;
+  }
+  const rolePing = "<@&" + role.id + "> ";
+  role
+    .setMentionable(true)
+    .then(async function (updated) {
+      await sleep(500);
+      interaction.reply(rolePing + message).then(async function () {
+        await sleep(500);
+        role.setMentionable(false);
+      });
+    })
+    .catch(console.error);
+};
+
 exports.run = async function (message, args, bot, db, roleName) {
   const events = message.channel.guild.roles.cache.find(
     (role) => role.name === roleName
