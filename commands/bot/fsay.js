48bbe6a83eb5390544214321c7f5e8d0d0414033
Rachel <16313442+EnchantedLuna@users.noreply.github.com>
2024-03-31 13:39:07 -0700

    update fsay

--- commands/bot/fsay.js
+++ commands/bot/fsay.js
@@ -1,6 +1,17 @@
-exports.run = function (message, params) {
-  message.channel.send(params);
-  if (message.deletable) {
-    message.delete();
+const config = require("../../config.json");
+
+exports.interaction = function (interaction, bot, db) {
+  if (interaction.user.id != config.botAdminUserId) {
+    interaction.reply({
+      content: "You do not have permission to run this command.",
+      ephemeral: true,
+    });
+    return;
   }
+  let message = interaction.options.getString("message");
+  interaction.channel.send(message);
+  interaction.reply({
+    content: "Message sent.",
+    ephemeral: true,
+  });
 };
