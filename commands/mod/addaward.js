bb16fb6891ec530fb64246f320ff95a0dd0da2bb
Rachel <16313442+EnchantedLuna@users.noreply.github.com>
2024-08-24 10:55:09 -0700

    remove !addaward

--- commands/mod/addaward.js
+++ commands/mod/addaward.js
@@ -1,9 +1,3 @@
-exports.run = function (message, args, bot, db) {
-  message.channel.send(
-    ":warning: This command must be used through the slash command /addaward"
-  );
-};
-
 exports.interaction = function (interaction, bot, db) {
   const member = interaction.options.getMember("member");
   const awardText = interaction.options.getString("text");
