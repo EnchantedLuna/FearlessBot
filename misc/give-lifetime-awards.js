6d84e1c77057182433f5d8f4ccd8612b37bd05db
Rachel <16313442+EnchantedLuna@users.noreply.github.com>
2025-02-18 18:43:53 -0800

    update lifetime lorpoint script to handle new seasonal difference

--- misc/give-lifetime-awards.js
+++ misc/give-lifetime-awards.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 const config = require("../config.json");
-const threshold = 100;
+const thresholds = [100, 200, 400];
 
 const db = mysql.createConnection({
   host: config.mysqlHost,
@@ -12,16 +12,23 @@ const db = mysql.createConnection({
 
 function giveAwards() {
   db.query(
-    "SELECT id, username, lorpoints FROM members WHERE server = ? AND lifetime_lorpoints > " +
-      threshold +
-      " AND active=1",
+    "SELECT id, username, lorpoints, lifetime_lorpoints FROM members WHERE server = ? AND lorpoints > 0 AND active=1",
     [config.mainServer],
     function (err, rows) {
       rows.forEach(function (member) {
-        db.query(
-          "INSERT INTO awards (server, member, award, date) VALUES (?,?,?,CURDATE())",
-          [config.mainServer, member.id, threshold + " Lifetime Lorpoints"]
-        );
+        const oldLifetimeTotal = member.lifetime_lorpoints;
+        const newLifetimeTotal = member.lifetime_lorpoints + member.lorpoints;
+        for (const threshold of thresholds) {
+          if (oldLifetimeTotal < threshold && newLifetimeTotal >= threshold) {
+            db.query(
+              "INSERT INTO awards (server, member, award, date) VALUES (?,?,?,CURDATE())",
+              [config.mainServer, member.id, threshold + " Lifetime Lorpoints"]
+            );
+            console.log(
+              member.username + " awarded for " + threshold + " lorpoints"
+            );
+          }
+        }
       });
     }
   );
