c040eeb82b57408315a6504fc11cbf863d7111fe
Rachel <16313442+EnchantedLuna@users.noreply.github.com>
2026-09-05 18:46:21 -0700

    fix: prevent banning other bots

--- commands/mod/idban.js
+++ commands/mod/idban.js
@@ -5,34 +5,52 @@ exports.interaction = function (interaction, bot, db) {
   const days = interaction.options.getInteger("days");
   if (isMod(userId, interaction.guild)) {
     interaction.reply(":smirk:");
-  } else {
-    interaction.guild.members
-      .ban(userId)
-      .then((user) => {
-        let responseText = `:hammer: Banned ${
-          user.username || user.id || user
-        }`;
-        if (days > 0) {
-          db.query(
-            "INSERT INTO scheduled_actions (action, guild, user, effectivetime) \
-                    VALUES ('unban', ?, ?, NOW() + INTERVAL ? DAY)",
-            [interaction.guild.id, userId, days]
-          );
-          responseText += ` for ${days} days`;
-        }
+    return;
+  }
+  interaction.guild.members
+    .fetch(userId)
+    .then((member) => {
+      if (member.user.bot) {
         interaction.reply({
           embeds: [
             {
-              description: responseText,
+              description:
+                ":warning: Sadly, I cannot ban another fellow robot. :robot:",
             },
           ],
         });
-      })
-      .catch((error) => {
-        interaction.reply(
-          ":no_entry: There was an error executing this operation."
-        );
-        console.error(error);
-      });
-  }
+      } else {
+        interaction.guild.members
+          .ban(userId)
+          .then((user) => {
+            let responseText = `:hammer: Banned ${
+              user.username || user.id || user
+            }`;
+            if (days > 0) {
+              db.query(
+                "INSERT INTO scheduled_actions (action, guild, user, effectivetime) \
+                        VALUES ('unban', ?, ?, NOW() + INTERVAL ? DAY)",
+                [interaction.guild.id, userId, days]
+              );
+              responseText += ` for ${days} days`;
+            }
+            interaction.reply({
+              embeds: [
+                {
+                  description: responseText,
+                },
+              ],
+            });
+          })
+          .catch((error) => {
+            interaction.reply(
+              ":no_entry: There was an error executing this operation."
+            );
+            console.error(error);
+          });
+      }
+    })
+    .catch((error) => {
+      interaction.reply(":no_entry: I could not find that member.");
+    });
 };
